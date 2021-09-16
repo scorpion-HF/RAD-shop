6486f0efb0cb95cf63a30b2d2e6d6fd19b4fd166
scorpion-HF <saeedalbooyeh@gmail.com>
2021-09-16 19:15:59 +0430

    email backend added temporary google smtp

--- rad_shop/rad_shop/settings.py
+++ rad_shop/rad_shop/settings.py
@@ -201,3 +201,10 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'saeedalbooyeh@gmail.com'
+EMAIL_HOST_PASSWORD = 'uuhtjxfubwcdsaxu'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'RAD Shopls'
